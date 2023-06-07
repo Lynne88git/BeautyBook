a2ffb5abfcef28cb9608050ceda02225ff282d62
Lynne Geldenhuys <geldenhuys.lynne@gmail.com>
2023-06-07 21:35:15 +0200

    first commit

---
+++ beautybook/webpack.config.ts
@@ -0,0 +1,83 @@
+import * as path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
+
+declare const __dirname: string;
+
+interface CustomConfiguration extends Configuration {
+  devServer?: {
+    port: number;
+    historyApiFallback: boolean;
+  };
+}
+
+const config: CustomConfiguration = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+const devServer = {
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
+
+export default { ...config, ...devServer };
